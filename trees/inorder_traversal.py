e9e6f87f7c160f61e236a6170813975dde7a1236
Yoni Keren <yoni.keren@gmail.com>
2025-05-27 23:11:49 -0400

    add iterative version for inorder traversal

--- trees/inorder_traversal.py
+++ trees/inorder_traversal.py
@@ -6,10 +6,19 @@
 #         self.right = right
 class Solution:
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        if root == None:
-            return []
-        left_inorder = self.inorderTraversal(root=root.left)
-        left_inorder.append(root.val)
-        right_inorder = self.inorderTraversal(root=root.right)
-        return left_inorder + right_inorder        
-        
+        #iterative solution will use a result list, as well as a stack of nodes
+        treeNodesToVisit = []
+        ans = []
+        while root or len(treeNodesToVisit)>0:
+            if root:
+                if root.left:
+                    treeNodesToVisit.append(root) #add to the stack current root, and visit its left subtree
+                    root = root.left
+                else: #no left subtree- I can visit the node right now,and its right subtree
+                    ans.append(root.val)
+                    root=root.right
+            else:
+                root = treeNodesToVisit.pop(-1) #i pop a node from the stack- those nodes had some left subtrees that i visited already
+                ans.append(root.val)
+                root=root.right
+        return ans
